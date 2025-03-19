f85ef9530c2b94194dbc5b8818a1619001eba160
Kibet <mshckkibet@gmail.com>
2025-03-19 09:17:46 +0300

    changed depricated import

--- backend/curriculum_processor.py
+++ backend/curriculum_processor.py
@@ -2,7 +2,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 from typing import List, Optional
 
 class CurriculumProcessor:
